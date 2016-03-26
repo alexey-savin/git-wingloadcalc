2dd3916ac79a48f49b967bd6b1c317d4091221aa
alexey-savin <alexeyv.savin@gmail.com>
2016-03-27 00:01:12 +0300

    added Windows 8.1 desktop support

---
+++ SSD.WingLoadCalculator/SSD.WingLoadCalculator.Shared/NotifyPropertyChangedBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace SSD.WingLoadCalculator
+{
+    class NotifyPropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            if (propertyName == null) throw new ArgumentNullException("propertyName");
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
